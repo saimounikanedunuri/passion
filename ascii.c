8eed8b37168bfad63446b8cd536f8e6da4d87ca5
saimounikanedunuri <saimounikanedunuri@gmail.com>
2017-03-09 08:20:59 -0800

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,11 @@
+/*ascii.c*/
+/*read cahr & display its ascii no. */
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	char ch;
+	printf("enter char");
+	ch=getchar();
+	printf("\nascii no of char %c is %d\n",ch,ch);
+}
